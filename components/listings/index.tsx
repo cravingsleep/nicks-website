92be9480ae910f1dc4240d9e517be8fa6003fa70
Nick Barker <nbarker@Nicks-MacBook-Pro.local>
2022-01-17 21:00:22 +0000

    Removed comment.

--- components/listings/index.tsx
+++ components/listings/index.tsx
@@ -19,45 +19,6 @@ function Listings() {
             logoAlt={listing.logo?.alt}
         />)}
     </ul>;
-
-    // return <ul className={styles.listings}>
-    //     <Listing
-    //         title="React"
-    //         description={react}
-    //         logoUrl="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
-    //         logoAlt="React logo"
-    //     />
-    //     <Listing
-    //         title="NodeJS"
-    //         description={nodejs}
-    //         logoUrl="https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg"
-    //         logoAlt="NodeJS logo"
-    //     />
-    //     <Listing
-    //         title="SCSS"
-    //         description={scss}
-    //         logoUrl="https://upload.wikimedia.org/wikipedia/commons/9/96/Sass_Logo_Color.svg"
-    //         logoAlt="SCSS logo"
-    //     />
-    //     <Listing
-    //         title="Jenkins"
-    //         description={jenkins}
-    //         logoUrl="https://upload.wikimedia.org/wikipedia/commons/e/e9/Jenkins_logo.svg"
-    //         logoAlt="Jenkins logo"
-    //     />
-    //     <Listing
-    //         title="Docker"
-    //         description={docker}
-    //     />
-    //     <Listing
-    //         title="Experience"
-    //         description={experience}
-    //     />
-    //     <Listing
-    //         title="Me"
-    //         description={aboutme}
-    //     />
-    // </ul>;
 }
 
 export default Listings;
